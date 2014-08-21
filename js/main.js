0131d32a4af0cab5f31df1302ea8b1f0dbcbd4d4
almossawi <almossawi@gmail.com>
2014-08-21 13:51:47 -0700

    Add histogram functionality

--- js/main.js
+++ js/main.js
@@ -6,14 +6,13 @@ $(document).ready(function() {
 
     //telemetry data for a subset of channels/versions for a chosen measure
     var telemetry_data = new Array();
-    
+
     //default time period (12 months)
     var past_n_days = 365;
-    
+
     var global = {};
     global.options = {
         'show-evolution-over': 'calendar-dates',
-        'selected_measure': ''
     }
 
     //array since we'll want it to be int-indexed (order is significant)
@@ -22,7 +21,7 @@ $(document).ready(function() {
         'nightly/33',
         'nightly/32'
     ];
-    
+
     //annotations
     var markers = [{
         'date': new Date('2014-06-13T00:00:00.000Z'),
@@ -36,7 +35,6 @@ $(document).ready(function() {
     //populate dropdown on first-load
     Telemetry.init(function() {
         var versions = Telemetry.versions();
-        console.log(versions);
 
         //get one of the versions (todo)
         var version = showing_releases[0];
@@ -71,28 +69,23 @@ $(document).ready(function() {
     //draw histogram and line chart
     function drawCharts() {
         telemetry_data = new Array();
-        
+
         Telemetry.init(function() {
-            var versions = Telemetry.versions();
+            //get evolution data for all three versions
+            $.each(showing_releases, function(i, version) {
+                //get measures for this version
+                Telemetry.measures(version, function(measures) {
+                    //set selected measure
+                    var selected_measure = $(".measure option:selected").text();
 
-            //get one of the versions (todo)
-            var version = showing_releases[0];
+                    //check that selected measure is valid
+                    if (measures[selected_measure] === undefined) {
+                        return;
+                    }
 
-            Telemetry.measures(version, function(measures) {
-                //set selected measure
-                global.options['selected_measure'] = $(".measure option:selected").text();
-
-                //check that selected measure is valid
-                if (measures[global.options['selected_measure']] === undefined) {
-                    return;
-                }
-
-                //get evolution data for all three versions
-                for(var i=0; i<showing_releases.length; i++) {
-                    var version = showing_releases[i];
-
-                    Telemetry.loadEvolutionOverTime(version, global.options['selected_measure'], function(histogramEvolution) {
+                    Telemetry.loadEvolutionOverTime(version, selected_measure, function(histogramEvolution) {
                         var data = [];
+
                         histogramEvolution.each(function(date, histogram) {
                             var total = 0;
                             histogram.map(function(count, start, end, index) {
@@ -102,25 +95,27 @@ $(document).ready(function() {
                             data.push({'date': date, 'value': total});
                             
                         });
-                        
+
                         //add this release's time-series data to telemetry_data
                         telemetry_data.push(data);
                         
                         //only plot, when we have the data for all releases loaded
                         check_everything(telemetry_data);
                     });
-                }
+                });
             })
-            
+
             //only plot, when we have the data for all releases loaded
             function check_everything(data) {
-                if (data.length == 3) {
+                if (data.length == showing_releases.length) {
                     plot_it(data);
                 }
             }
 
             //draw evolution chart
-            function plot_it(data) {console.log(data);
+            function plot_it(data) {
+                console.log(data);
+                
                 //draw the chart on first-load
                 split_by_data = moz_chart({
                     title: "Submissions",
@@ -141,50 +136,72 @@ $(document).ready(function() {
                     y_accessor: 'value'
                 })
             }
-            
+
             //draw histogram
-            drawHistogram({title: showing_releases[0]});
-            
+            setTimeout(function() {
+                drawHistogram({title: showing_releases[0], sequence: 1});
+            }, 50);
+
             //default color for histogram
             d3.selectAll(".bar rect")
                 .classed('area1-color', true);
-        })// end Telemetry.init
-    }
-    
-    //draw histogram
-    function drawHistogram(options) {
-        var title = (options.title) ? options.title : 'Histogram';
-
-        //generate a Bates distribution of 10 random variables
-        var values = d3.range(10000).map(d3.random.bates(10));
-        var x = d3.scale.linear()
-            .domain([0, 1])
-            .range([0, 350 - 0 - 10]);
-        
-        moz_chart({
-            title: title,
-            description: "A histogram of the buckets for the chosen measure conditioned on release.",
-            data: values,
-            chart_type: 'histogram',
-            width: 550,
-            height: 389,
-            left: 30,
-            right: 10,
-            target: '#histogram',
-            y_extended_ticks: true,
-            xax_count: 10,
-            xax_tick: 5,
-            bins: 50,
-            bar_margin: 2,
-            rollover_callback: function(d, i) {
-                $('#histogram svg .active_datapoint')
-                    .html('Value: ' + d3.round(d.x,2) +  '   Count: ' + d.y);
-            },
-            x_accessor: 'x',
-            y_accessor: 'y'
         })
     }
-    
+
+    //draw histogram
+    function drawHistogram(options) {
+        var version = (options.title) ? options.title : 'Histogram';
+        var selected_measure = $(".measure option:selected").text();
+        
+        Telemetry.init(function() {
+            //get evolution data for the selected version
+            Telemetry.loadEvolutionOverTime(version, selected_measure, function(histogramEvolution) {
+                var data = [];
+                var histogram = histogramEvolution.range();
+
+                //get the buckets
+                histogram.each(function(count, start, end, index) {
+                    data[index] = {x: index, y: count};
+                });
+                
+                var y_scale_type = 
+                    (histogram.kind() == 'enumerated' || histogram.kind() == 'exponential')
+                        ? 'log'
+                        : 'linear';
+
+                //draw the histogram
+                moz_chart({
+                    title: version,
+                    description: "A histogram of the buckets for the chosen measure conditioned on release.",
+                    data: data,
+                    chart_type: 'histogram',
+                    width: 550,
+                    height: 389,
+                    left: 40,
+                    right: 40,
+                    y_scale_type: 'log',
+                    target: '#histogram',
+                    y_extended_ticks: true,
+                    xax_count: 10,
+                    xax_tick: 5,
+                    bar_margin: 2,
+                    binned: true,
+                    rollover_callback: function(d, i) {
+                        $('#histogram svg .active_datapoint')
+                            .html('Value: ' + d3.round(d.x,2) +  '   Count: ' + d.y);
+                    },
+                    x_accessor: 'x',
+                    y_accessor: 'y'
+                })   
+                
+                
+                //update histogram color
+                d3.selectAll(".bar rect")
+                    .classed('area' + options.sequence + '-color', true);   
+            });
+        })
+    }
+
     //remove disabled lines from the chart
     function filterOutDisabledReleases() {
         var data = [];
@@ -196,7 +213,7 @@ $(document).ready(function() {
 
             data.push(telemetry_data[i]);
         }
-        
+
         //check if we need to constrain by time before returning
         //we need to constrain by past_n_days of latest release only
         data = modify_time_period(data, past_n_days)
@@ -208,7 +225,7 @@ $(document).ready(function() {
     //pattern of our lines, e.g. if line is disabled, return [2,3]
     function customerLineToColorMap() {
         var data = [];
-        
+
         var j = 1; //index for assigning line color to line
         for(var i=0; i<showing_releases.length; i++) {
             if(showing_releases[i] == '') {
@@ -222,7 +239,7 @@ $(document).ready(function() {
 
         return data;
     }
-    
+
     function numOfEnabledReleases() {
         //var releases = d3.entries(showing_releases);
         var count = 0;
@@ -230,46 +247,41 @@ $(document).ready(function() {
 
         return count;
     }
-    
+
     function assignEventListeners() {
         //switch to release
         $('.btn-release').click(function () {
             var chosen_i = $(this).data('sequence')
-            
+
             //if button is disabled, all bets are off
             if($(this).attr('class') == 'disabled')
                 return;
-            
-            //redraw histogram using dummy date
-            drawHistogram({title: $(this).text()});
-            
-            //update histogram color
-            d3.selectAll(".bar rect")
-                .classed('area' + chosen_i + '-color', true);
-            
+
+            //redraw histogram
+            drawHistogram({title: $(this).text(), sequence: chosen_i});
+
             //reset all widths
             $('.main-line')
                 .css('stroke-width', '1.1px');
-                
+
             //make selected line thicker
             $('.main-line.line' + chosen_i + '-color')
                 .css('stroke-width', '1.8px');
-            
+
             return false;
         })
-        
+
         //enable/disable release
         $('.disable-enable').click(function () {
             var chosen_i = $(this).data('sequence');
             var chosen_release = $(this).data('release');
-            
+
             //update label and dropdown's opacity
             var label = $($(this).children()[0]).text();
-            
+
             if(label == 'Enable') {
                 //add release to our set of releases
                 showing_releases[chosen_i-1] = chosen_release;
-                console.log(showing_releases);
 
                 //enable this release
                 $($(this).children()[0]).html("Disable");
@@ -298,16 +310,14 @@ $(document).ready(function() {
                     
                     return false;
                 }
-                
+
                 //remove release from our set of releases
                 showing_releases[chosen_i-1] = '';
-                console.log(showing_releases);
-            
+
                 //disable this release
                 $($(this).children()[0]).html("Enable");
                 d3.select('.btn-release.line' + chosen_i + '-legend-box-color')
                     .classed('disabled', true);
-                    
 
                 $('.btn-release.line' + chosen_i + '-legend-box-color')
                     .css('opacity', 0.5);
@@ -315,35 +325,35 @@ $(document).ready(function() {
                 $('.btn-release-options.line' + chosen_i + '-legend-box-color')
                     .css('opacity', 0.5);
             }
-            
+
             //update data    
             //remove disabled one from data
             updateDataMySon({});
 
             return false;
         })
-        
+
         //telemetry measure
         $('.measure').click(function () {
             var chosen_measure = $(this).val();
             console.log(chosen_measure);
-            
+
             //redraw histogram and line chart
             drawCharts();
         });
-        
+
         //preferences
         $('.options').click(function () {
             var chosen_option = $(this).attr('class').split(' ')[1];
             var chosen_option_value = $(this).val();
             console.log(chosen_option, chosen_option_value);
-            
+
             //TODO do other stuff to update chart with new option
-            
+
             //if we're switching the x-axis to build ids, reformat the labels
             if(chosen_option_value == 'build-ids') {
                 global.options['show-evolution-over'] = 'build-ids';
-                
+
                 //update data
                 updateDataMySon({
                     show_years: false
@@ -352,17 +362,17 @@ $(document).ready(function() {
             //if we're switching the x-axis to calendar dates, reformat the labels
             else if(chosen_option_value == 'calendar-dates') {
                 global.options['show-evolution-over'] = 'calendar-dates';
-            
+
                 updateDataMySon({});
             }
         });
-        
+
         //filter
         $('.filter').click(function () {
             var chosen_filter = $(this).attr('class').split(' ')[1];
             var chosen_filter_value = $(this).val();
             console.log(chosen_filter, chosen_filter_value);
-            
+
             //TODO do other stuff to update chart with new filters
             //TODO repopulate other dropdowns if necessary, e.g. OS version on OS change
         });
@@ -372,56 +382,55 @@ $(document).ready(function() {
             var chosen_release = $(this).val();
             var chosen_sequence = $($(".open").children()[0]).data('sequence');
             console.log(chosen_release, chosen_sequence);
-            
+
             //TODO updated the button with the newly selected release
             //TODO do other stuff to update chart
         });
-            
+
         //preferences
         $('.preferences').click(function () {
             var chosen_i = $(this).data('sequence')
-            
+
             //dim the background
             $(".dim").show();
 
             //show the modal box
             $("#preferences-box").show();
-        
+
             return false;
         });
         
         //advanced filter
         $('.advanced-filter').click(function () {
             var chosen_i = $(this).data('sequence')
-            
+
             //dim the background
             $(".dim").show();
 
             //show the modal box
             $("#advanced-filter-box").show();
-        
+
             return false;
         });
-           
-           
+
         //change release
         $('.change').click(function () {
             var chosen_i = $(this).data('sequence')
-            
+
             //dim the background
             $(".dim").show();
 
             //show the modal box
             $("#choose-releases-box").show();
-            
+
             return false;
         })
-        
+
         //modal box event listeners
         $(".close_modal_box").click(function(e) {
             $(".dim").hide();
             $(".modal").hide();
-            
+
             return false;
         });
 
@@ -437,7 +446,7 @@ $(document).ready(function() {
                 $(".modal").hide();
             }
         };
-        
+
         //switch time period
         $('.modify-time-period-controls button').click(function() {
             //update our time period global variable
@@ -451,41 +460,41 @@ $(document).ready(function() {
             //update data
             updateDataMySon({});
         })
-        
+
         //switch between light and dark themes
         $('#dark-css').click(function () {
             $('.missing')
                 .css('background-image', 'url(images/missing-data-dark.png)');
-                
+
             $('.transparent-rollover-rect')
                 .attr('fill', 'white');
-        
+
             $('.pill').removeClass('active');
             $(this).toggleClass('active');
-            
+
             $('#dark').attr({href : 'css/metrics-graphics-darkness.css'});
             $('#dark-telemetry').attr({href : 'css/telemetry-darkness.css'});
-            
+
             return false;
         })
 
         $('#light-css').click(function () {
             $('.missing')
                 .css('background-image', 'url(images/missing-data.png)');
-                
+
             $('.transparent-rollover-rect')
                 .attr('fill', 'black');
-        
+
             $('.pill').removeClass('active');
             $(this).toggleClass('active');
-            
+
             $('#dark').attr({href : ''});
             $('#dark-telemetry').attr({href : ''});
             $('#light-telemetry').attr({href : 'css/telemetry.css'});
             return false;
         })
     }
-    
+
     //update data
     function updateDataMySon(options) {
         //don't show years for build ids or if we explicitly pass that in as an option
@@ -493,12 +502,12 @@ $(document).ready(function() {
             || global.options['show-evolution-over'] == 'build-ids')
                 ? false
                 : true;
-                
+
         var x_label = (options.x_label !== undefined 
             || global.options['show-evolution-over'] == 'build-ids')
                 ? 'Build IDs'
                 : '';
-                    
+
         //call moz_chart, taking into consideration options and filters that 
         //the user has set
         moz_chart({
